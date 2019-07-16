41ca3eff34a0add7ca8f2aa920b31e2e004eb238
dcais <dyingjie_cai@163.com>
2019-07-16 16:54:25 +0800

    commit

--- stock/entry-point.sh
+++ stock/entry-point.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.security.egd=file:/dev/./urandom $JVM_OPTS -jar /config-server.jar --spring.config.location=${CONFIG_LOCATION} ${APP_OPTS}
+java -Djava.security.egd=file:/dev/./urandom $JVM_OPTS -jar /${EXEC_TARGET_JAR} --spring.config.location=${CONFIG_LOCATION} ${APP_OPTS}
