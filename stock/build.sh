49d489fed1fba755589546142998dc7dacc27c5b
dcais <dyingjie_cai@163.com>
2019-07-17 09:50:41 +0800

    update

--- stock/build.sh
+++ stock/build.sh
@@ -9,8 +9,11 @@ mvn -U clean package
 JAR_FILE=$(ls -al target/ | grep 'stock.*\.jar$'  | awk '{print $9}')
 STOCK_VER=$(ls -al target/ | grep 'stock.*\.jar$'  | awk '{print $9}' | sed 's/stock-//g' | sed 's/\.jar//g')
 
+rm -fr ${DOCKER_BUILD_PATH}/jar/*.jar
 cp ./target/${JAR_FILE} ${DOCKER_BUILD_PATH}/jar/
 
 cd ${DOCKER_BUILD_PATH}
+
+sed -i "" "s/stock-.*\.jar/${JAR_FILE}/g" Dockerfile
 
 # docker build -t dockersai/stock:v${STOCK_VER} --uild-arg STOCK_VER=${STOCK_VER} .
